95e03367e1afd92e5fbd37303e98996e50a8e587
bighandsdev <63073747+bighandsdev@users.noreply.github.com>
2020-08-02 13:17:26 +0100

    readability

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import CustomizedTables from './components/CustomizedTables.js';
 import Cards from './components/Cards.js';
 import SearchBar from './components/SearchBar.js';
 
-//#region
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -31,9 +30,6 @@ class App extends React.Component {
             coins: result,
           });
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
         (error) => {
           this.setState({
             isLoaded: true,
@@ -43,33 +39,19 @@ class App extends React.Component {
       );
   }
   handleChange(e) {
-    // Variable to hold the original version of the list
     let currentList = [];
-    // Variable to hold the filtered list before putting into state
     let newList = [];
-
-    // If the search bar isn't empty
     if (e !== '') {
-      // Assign the original list to currentList
       currentList = this.state.coins;
 
-      // Use .filter() to determine which items should be displayed
-      // based on the search terms
       newList = currentList.filter((coin) => {
-        // change current item to lowercase
         const lc = coin.toString().toLowerCase();
-        // change search term to lowercase
         const filter = e.toLowerCase();
-        // check to see if the current list item includes the search term
-        // If it does, it will be added to newList. Using lowercase eliminates
-        // issues with capitalization in search terms and search content
         return lc.includes(filter);
       });
     } else {
-      // If the search bar is empty, set newList to original task list
       newList = this.state.coins;
     }
-    // Set the filtered state based on what our rules added to newList
     this.setState({
       coins: newList,
     });
